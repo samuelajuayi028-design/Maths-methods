f5e5b099eeb514acbd5cb740266e8aa58f18ef0c
samuelajuayi028-design <samuelajuayi028@gmail.com>
2026-01-24 17:36:59 +0100

    first commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+ System.out.println(Math.max(5, 10));
+ System.out.println(Math.min(5,10));
+ System.out.println(Math.sqrt(64));
+    }
+}
