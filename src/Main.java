1e51f371baf0a989edd60a2f4a107e7fc33edb44
samuelajuayi028-design <samuelajuayi028@gmail.com>
2026-01-30 18:15:13 +0100

    second commit

--- src/Main.java
+++ src/Main.java
@@ -2,9 +2,75 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
- System.out.println(Math.max(5, 10));
- System.out.println(Math.min(5,10));
- System.out.println(Math.sqrt(64));
-        System.out.println("This is maths method class");
-    }
-}
+        boolean isRaining = true;
+
+        int x = 20;
+        int y = 18;
+
+        if (isRaining) {
+            System.out.println("Bring an umbrella!");
+        }
+
+        if (20 > 18) {
+            System.out.println("20 is greater than 18");
+        }
+
+        int a = 20;
+        int b = 18;
+        if ( a > b) {
+            System.out.println("x is greater than y");
+
+        boolean isLightOn = true;
+        if (isLightOn) {
+
+
+
+        boolean IsLightOn = false;
+        if (isLightOn) {
+            System.out.println("The light is on.");  // This will not be printed
+            System.out.println("This line always runs.");
+
+
+            if (20 > 18)
+                System.out.println("20 is greater than 18");
+
+
+            int X = 20;
+            int Y = 18;
+
+            if (x > y)
+                System.out.println("x is greater than y");
+            System.out.println("This line runs no matter what (not part of the if statement)");
+
+
+            int X = 20;
+            int Y = 18;
+
+            if (x > y) {
+                System.out.println("x is greater than y");
+                System.out.println("Both lines are part of the if");
+            }
+
+            System.out.println("I am outside if, not part of if!");
+
+
+            boolean IsRaining = false;
+
+            if (isRaining) {
+                System.out.println("Bring an umbrella!");
+            } else {
+                System.out.println("No rain today, no need for an umbrella!");
+
+
+                int time = 20;
+                if (time < 18) {
+                    System.out.println("Good day.");
+                } else {
+                    System.out.println("Good evening.");
+
+
+
+
+                }
+            }
+        }
